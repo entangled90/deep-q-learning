a9534d4d3585b7f0bf95efedf1bc24b8da874cfd
Carlo <sana.carlo@gmail.com>
2021-01-31 18:18:27 +0100

    fixed issues

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -7,7 +7,7 @@ fi
 
 echo "opening notebook with folder in $folder"
   
-sudo docker run --gpus all -it -p 8888:8888 -v $(realpath $folder):/tf/notebooks tensorflow-jupyter bash
+docker run --gpus all -it -p 8888:8888 -v $(realpath $folder):/tf/notebooks tensorflow-jupyter 
 
 # command to start in docker:
 # xvfb-run -s "-screen 0 1400x900x24" jupyter notebook --ip 0.0.0.0 --allow-root 
